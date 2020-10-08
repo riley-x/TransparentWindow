f9057e703568399157d6debb82566babd8573893
Riley Xu <riley.xu@gmail.com>
2020-10-08 16:43:59 -0400

    Add project files.

---
+++ TransparentWindow/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <windows.h>
+#pragma comment(lib, "user32.lib")
+#include <wrl.h>
+#include <dxgi1_3.h>
+#include <d3d11_2.h>
+#include <d2d1_2.h>
+#include <d2d1_2helper.h>
+#include <dcomp.h>
+#pragma comment(lib, "dxgi")
+#pragma comment(lib, "d3d11")
+#pragma comment(lib, "d2d1")
+#pragma comment(lib, "dcomp")
